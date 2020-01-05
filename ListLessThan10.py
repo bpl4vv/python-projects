7e24faf1716143320a71cb827de8ae62f4314919
Ben <bpl4vv@virginia.edu>
2020-01-05 18:56:43 -0500

    Cleaned up programs

--- ListLessThan10.py
+++ ListLessThan10.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Take a list, say for example this one:
 # a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # and write a program that prints out all the elements of the list that are less than 5.
@@ -41,48 +40,3 @@ for i in range(num):
 printLess(List)
 extra1printLess(List)
 extra3printLess(List)
-=======
-# Take a list, say for example this one:
-# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# and write a program that prints out all the elements of the list that are less than 5.
-
-# Extras:
-# 1. Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it
-# and print out this new list.
-# 2. Write this in one line of Python. UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUH HOW TO DO
-# 3. Ask the user for a number and return a list that contains only elements from the original list a
-# that are smaller than that number given by the user.
-
-def printLess(list):
-    for element in list:
-        if element < 5:
-            print(element)
-
-def extra1printLess(list1):
-    result = []
-    for element in list1:
-        if element < 5:
-            result.append(element)
-    print(result)
-
-def extra3printLess(list3):
-    result = []
-    print("Number to print elements less than?")
-    cap = int(input())
-    for element in list3:
-        if element < cap:
-            result.append(element)
-    print(result)
-
-print("Give me a number.")
-num = int(input())
-print("Give me", num, "elements.")
-List = []
-for i in range(num):
-    List.append(int(input()))
-
-printLess(List)
-extra1printLess(List)
-extra3printLess(List)
->>>>>>> 50f0fd09e2fc0bf4c895e062dc0d5235955a15ad
-print(List[0])
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -6,27 +6,40 @@
 # Scissors beats paper
 # Paper beats rock
 
+
 def comp(move1, move2):
-    if(move1 == move2):
+    if move1 == move2:
         print("It's a tie!")
-    elif(move1 == "rock"):
-        if(move2 == "scissors"):
+    elif move1 == "rock":
+        if move2 == "scissors":
             return True
         else:
             return False
-    elif(move1 == "paper"):
-        if(move2 == "rock"):
+    elif move1 == "paper":
+        if move2 == "rock":
             return True
         else:
             return False
-    elif(move1 == "scissors"):
-        if(move2 == "paper"):
+    else:
+        if move2 == "paper":
             return True
         else:
             return False
 
 print("Welcome to Rock-Paper-Scissors! This is a 2-player game. Input your moves.")
-p1 = input("Player 1: ")
-p2 = input("Player 2: ")
-if(p1,p2 != "rock","paper","scissors"):
-    
+q = "" #input if users want to quit
+while q != "quit":
+    p1 = input("Player 1: ")
+    p2 = input("Player 2: ")
+    while(not(p1 == "rock" or p1 == "paper" or p1 == "scissors" or
+            p2 == "rock" or p2 == "paper" or p2 == "scissors")):
+        print("Someone picked a nonexistent move! Pick again.")
+        p1 = input("Player 1: ")
+        p2 = input("Player 2: ")
+    if(comp(p1,p2)):
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
+    print("Enter 'quit' to quit. Enter anything else to play again.")
+    q = input()
+print("Thanks for playing!")
